dad9c3128b7f1811fa6949d156cc65d8c14c3971
Yelzhas Kassymbayev <102794244+yelzhaskk@users.noreply.github.com>
2022-10-03 10:16:03 +0600

    1

--- main.py
+++ main.py
@@ -63,4 +63,11 @@
 #     else:
 #         return n+sum_numbers(n-1)
 # print(sum_numbers(int(input("Enter number: "))))
+#
 
+# def is_triplet(a,b,c):
+#     if (a**2)+(b**2)==(c**2) or (a**2)+(c**2)==(b**2) or (c**2)+(b**2)==(a**2):
+#         print("True")
+#     else:
+#         print("False")
+# is_triplet(a = int(input("a: ")),b = int(input("b: ")),c = int(input("c: ")))
