27f7686d04a1fc714e887d5b33ef97646e57af5a
Yelzhas Kassymbayev <102794244+yelzhaskk@users.noreply.github.com>
2022-10-03 10:17:05 +0600

    home

--- main.py
+++ main.py
@@ -56,15 +56,7 @@
 # sum = get_Discount(*map(int,input("Enter numbers: ").split()))
 # print(sum)
 
-#2
-# def sum_numbers(n):
-#     if n == 0:
-#         return 0
-#     else:
-#         return n+sum_numbers(n-1)
-# print(sum_numbers(int(input("Enter number: "))))
-#
-
+#3
 # def is_triplet(a,b,c):
 #     if (a**2)+(b**2)==(c**2) or (a**2)+(c**2)==(b**2) or (c**2)+(b**2)==(a**2):
 #         print("True")
